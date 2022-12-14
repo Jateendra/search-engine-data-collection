2f9e148cfaf2386a61bdd647d63d4d14ea989936
Jateendra <jateendra.pradhan@gmail.com>
2022-12-14 19:28:04 +0530

    updated

--- src/utils/database_handler.py
+++ src/utils/database_handler.py
@@ -8,8 +8,11 @@ class MongodbClient:
     def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
         if MongodbClient.client is None:
             MongodbClient.client = pymongo.MongoClient(
-                f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+                # f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+                f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@cluster-jateen.rwub0dz.mongodb.net/?retryWrites=true&w=majority"
             )
         self.client = MongodbClient.client
         self.database = self.client[database_name]
+        print("self.database : ",self.database)
         self.database_name = database_name
+        print("self.database_name : ",self.database_name)
